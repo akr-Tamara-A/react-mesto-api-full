357c2563a59eda9a87ecc359be6ec265bae6c79e
Tamara A <akira-49-q@yandex.ru>
2020-11-27 17:09:15 +0200

    fix: изменить url для запросов

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'http://api.akr.students.nomoreparties.space';
 
 /** Запрос регистрации пользователя */
 export const register = (email, password) => {
